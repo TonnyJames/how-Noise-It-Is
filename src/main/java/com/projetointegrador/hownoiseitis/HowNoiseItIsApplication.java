2b4e56d7521f28c47d73adbc9c80079247c66c8e
Tonny James P. dos Reis <86257095+TonnyJames@users.noreply.github.com>
2023-10-13 17:12:48 -0300

    Iniciando projeto

---
+++ src/main/java/com/projetointegrador/hownoiseitis/HowNoiseItIsApplication.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.hownoiseitis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowNoiseItIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowNoiseItIsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projetointegrador/hownoiseitis/HowNoiseItIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.hownoiseitis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowNoiseItIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
