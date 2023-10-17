f8185bbfb7850d8f6f6bf94fe15e88e88c0c3ae4
Tonny James P. dos Reis <86257095+TonnyJames@users.noreply.github.com>
2023-10-16 22:27:35 -0300

    entidade para recebimento de informações do dispositivo de som

---
+++ src/main/java/com/projetointegrador/hownoiseitis/entities/InfoDb.java
@@ -0,0 +1,74 @@
+package com.projetointegrador.hownoiseitis.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Document
+public class InfoDb {
+
+    @Id
+    private String id;
+    private String nomeDispositivo;
+    @JsonFormat(pattern = "dd-MM-yyyy@HH:mm:ss", locale = "pt_BR")
+    private LocalDateTime data;
+    private Integer valor;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNomeDispositivo() {
+        return nomeDispositivo;
+    }
+
+    public void setNomeDispositivo(String nomeDispositivo) {
+        this.nomeDispositivo = nomeDispositivo;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public Integer getValor() {
+        return valor;
+    }
+
+    public void setValor(Integer valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InfoDb infoDb = (InfoDb) o;
+        return Objects.equals(id, infoDb.id) && Objects.equals(nomeDispositivo, infoDb.nomeDispositivo) && Objects.equals(data, infoDb.data) && Objects.equals(valor, infoDb.valor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nomeDispositivo, data, valor);
+    }
+
+    @Override
+    public String toString() {
+        return "InfoDb{" +
+                "id='" + id + '\'' +
+                ", nomeDispositivo='" + nomeDispositivo + '\'' +
+                ", data=" + data +
+                ", valor=" + valor +
+                '}';
+    }
+}
