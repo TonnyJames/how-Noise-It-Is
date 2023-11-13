a2d7ccd42b50f400fbddd6e7350db97afb300e2c
Tonny James P. dos Reis <86257095+TonnyJames@users.noreply.github.com>
2023-11-13 16:26:28 -0300

    lógica para buscar eventos de apenas dois dias atrás

--- src/main/java/com/projetointegrador/hownoiseitis/repositories/MongoEventsRepository.java
+++ src/main/java/com/projetointegrador/hownoiseitis/repositories/MongoEventsRepository.java
@@ -1,9 +1,13 @@
 package com.projetointegrador.hownoiseitis.repositories;
 
 import com.projetointegrador.hownoiseitis.entities.InfoDb;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 @Repository
 public class MongoEventsRepository extends MongoPersistence {
@@ -13,4 +17,23 @@ public class MongoEventsRepository extends MongoPersistence {
         return getMongoTemplate().save(infoDb, "infoDecibeis");
     }
 
+    public Object consultarUltimasMedicoes() {
+
+        LocalDateTime horaAtual = LocalDateTime.now();
+
+        LocalDateTime dataHoraModificado = horaAtual.minusDays(2);
+
+        Query query = new Query().addCriteria(Criteria.where("data").gte(dataHoraModificado).lte(horaAtual));
+
+        return getMongoTemplate().find(query, InfoDb.class, "infoDecibeis");
+
+    }
+
+    public Object consultarUltimaMedicao() {
+
+        Query query = new Query();
+        query.limit(1);
+        query.with(Sort.by(Sort.Direction.DESC,"_id"));
+        return getMongoTemplate().findOne(query, InfoDb.class, "infoDecibeis");
+    }
 }
